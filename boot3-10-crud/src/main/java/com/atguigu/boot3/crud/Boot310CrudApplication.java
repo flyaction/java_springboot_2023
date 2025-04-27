0823d25c6d2317c691db558594767480828d2b80
action <xingdong1117@126.com>
2025-04-27 09:05:23 +0800

    fix:swagger-ui

--- boot3-10-crud/src/main/java/com/atguigu/boot3/crud/Boot310CrudApplication.java
+++ boot3-10-crud/src/main/java/com/atguigu/boot3/crud/Boot310CrudApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Boot310CrudApplication {
 
+    //http://localhost:9000/swagger-ui/index.html#/
     public static void main(String[] args) {
         SpringApplication.run(Boot310CrudApplication.class, args);
     }
